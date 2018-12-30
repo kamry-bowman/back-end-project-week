1eea0f53a5abd65d42d43ad1a6e7d03cfacc8ca6
Kamry Bowman <kamry.bowman@gmail.com>
2018-12-30 15:38:51 -0700

    Update note seed with more appropriate examples

--- dummyData/noteSeedArray.js
+++ dummyData/noteSeedArray.js
@@ -1,55 +1,61 @@
 module.exports = [
   {
-    title: 'Stuff you learned',
-    text_body: 'There are so many important things you have learned.',
+    title: 'Var hoisting',
+    text_body:
+      'For the var declaration, the javascript interpreter hoists variables within a scope, knowing that they exist, but not what they are defined as.',
     created_at: '2018-08-01 5:45:12',
     user_id: 1,
     left: -1,
     right: 2,
   },
   {
-    title: 'You never forget anything',
-    text_body: "There's a time and a place.",
+    title: 'Array Creation',
+    text_body:
+      'In javascript, Array.from({ length: n }) will create an array with n empty items. More stuff',
     created_at: '2018-08-02 7:45:12',
     user_id: 1,
     left: 1,
     right: 3,
   },
   {
-    title: 'Cigarettes Kill',
-    text_body: '98% of smokers are soldiers.',
+    title: 'Bash',
+    text_body:
+      "In Bash, the piping operator sends a program's output directly as input to another program",
     created_at: '2018-08-05 5:35:12',
     user_id: 1,
     left: 2,
     right: 4,
   },
   {
-    title: 'Originally Fifth',
-    text_body: '08% of smokers are soldiers.',
+    title: 'Python',
+    text_body:
+      'In Python, adding *name as a function parameter after all other positional arguments will create a dictionary that takes all remaining positional input arguments as its members.',
     created_at: '2018-08-05 5:35:12',
     user_id: 1,
     left: 3,
     right: 5,
   },
   {
-    title: 'Originally Sixth',
-    text_body: '08% of smokers are soldiers.',
+    title: 'Python',
+    text_body:
+      'In Python, a for loop can take an else block. The else block will execute if the for loop executes naturally without a break',
     created_at: '2018-08-05 5:35:12',
     user_id: 1,
     left: 4,
     right: -1,
   },
   {
-    title: 'Stuff you learned for user2',
-    text_body: 'There are so many important things you have learned.',
+    title: 'Javascript',
+    text_body:
+      'Properties with keys as symbols are not included in getOwnPropertyNames calls',
     created_at: '2018-08-01 5:45:12',
     user_id: 2,
     left: -1,
     right: 7,
   },
   {
-    title: 'You never forget anything for user2',
-    text_body: "There's a time and a place.",
+    title: 'Postresql',
+    text_body: 'Posgresql features the enum data type.',
     created_at: '2018-08-02 7:45:12',
     user_id: 2,
     left: 6,
